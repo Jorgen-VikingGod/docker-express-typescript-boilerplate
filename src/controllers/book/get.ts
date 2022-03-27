203d963d43ce184675e93ca95d13089a8bbaa6b4
Juergen Skrotzky <JorgenVikingGod@gmail.com>
2022-03-27 18:44:49 +0200

    Update example

--- src/controllers/book/get.ts
+++ src/controllers/book/get.ts
@@ -5,10 +5,17 @@ import logger from '../../logger';
 
 const get: RequestHandler = async (req: Request, res) => {
   const { bookId } = req.params;
+  logger.error(`Book to get: ${bookId}`);
+  logger.warn(`Book to get: ${bookId}`);
+  logger.info(`Book to get: ${bookId}`);
+  logger.verbose(`Book to get: ${bookId}`);
+  logger.debug(`Book to get: ${bookId}`);
   logger.silly(`Book to get: ${bookId}`);
+  
 
   // const book = await Book.findById(bookId);
   const book = {name: 'test1', author: 'test1'};
+  logger.silly(`Book get response:\n${JSON.stringify(book, null, 2)}`);
   if (!book) {
     return res.status(404).send({
       error: 'Book not found'
